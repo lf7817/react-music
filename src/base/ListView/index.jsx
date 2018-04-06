6faf2e0a815a23af71ce128b323a603dc4a431ee
lf7817 <535536456@qq.com>
2018-04-06 17:00:55 +0800

    备份

--- src/base/ListView/index.jsx
+++ src/base/ListView/index.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { observable } from 'mobx'
-import { observer } from 'mobx-react'
+import { observer, action } from 'mobx-react'
 import Scroll from '@/base/Scroll'
 import { getData } from '@/common/dom'
 
 import './style.styl'
 
+const ITEM_HEIGHT = 18
 
 @observer
 class ListView extends Component {
@@ -19,24 +20,58 @@ class ListView extends Component {
     list: []
   }
 
-  @observable currentIndex = 0
+  @observable currentIndex = 0;
+  @observable scrollY = 0;
+  
+  touch = {};
 
   onTouchStart = e => {
+    const touches = e.touches[0]
     const index = getData(e.target, 'index')
     const ele = this.listGroup.children[index]
-    this.scroll.scrollToElement(ele, 0)
+    this.touch.startY = touches.pageY
+    this.touch.anchorIndex = index
+    this.scrollTo(ele, 0)
     e.stopPropagation()
   }
 
   onTouchMove = e => {
+    const touches = e.touches[0]
+    const delta = Math.round((touches.pageY - this.touch.startY) / ITEM_HEIGHT)
+    const index = parseInt(this.touch.anchorIndex, 10) + delta
+    this.scrollTo(index)
     e.stopPropagation()
+  }
+  
+  onScroll = e => {
+    this.scrollY = e.y
+  }
+
+  scrollTo (index) {
+    const len = this.listGroup.children.length
+    if (!index && index !== 0) {
+      return
+    }
+    if (index < 0) {
+      index = 0
+    } else if (index >= len - 1) {
+      index = len - 1
+    }
+    
+    console.log(index)
+    const ele = this.listGroup.children[index]
+    this.scroll.scrollToElement(ele, 0)
   }
 
   render () {
     const list = this.props.list
 
     return (
-      <Scroll className="list-view" ref={scroll => this.scroll = scroll}>
+      <Scroll 
+        probeType={3}
+        className="list-view" 
+        onScroll={this.onScroll}
+        ref={scroll => this.scroll = scroll}>
         <ul ref={listGroup => this.listGroup = listGroup}>
           {
             list.length !== 0 ? (
