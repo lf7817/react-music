aed87786dc427d77782e6a14688042bb1b91820d
lf7817 <535536456@qq.com>
2018-04-11 10:08:42 +0800

    备份

--- src/base/ListView/index.jsx
+++ src/base/ListView/index.jsx
@@ -5,53 +5,93 @@ import { getData } from '@/common/dom'
 
 import './style.styl'
 
-// const ITEM_HEIGHT = 18
+const ITEM_HEIGHT = 18
 
 class ListView extends Component {
   static propTypes = {
     list: PropTypes.array.isRequired
-  }
+  };
 
   static defaultProps = {
     list: []
-  }
+  };
 
   touch = {
     startIndex: -1
-  }
+  };
+
+  listItemHeights = [];
 
   state ={
     index: 0
-  }
+  };
 
   touchStartHandler = e => {
+    const touch = e.touches[0]
     const touchStartIndex = parseInt(getData(e.target, 'index'), 10)
     this.touch.startIndex = touchStartIndex
-    this.setState({
-      index: touchStartIndex
-    })
+    this.touch.startY = touch.pageY
+    this.scrollTo(touchStartIndex)
     e.stopPropagation()
+  }
+
+  touchMoveHandler = e => {
+    const touch = e.touches[0]
+    const delta = Math.round((touch.pageY - this.touch.startY) / ITEM_HEIGHT)
+    const anchorIndex = this.touch.startIndex + delta
+    this.scrollTo(anchorIndex)
+    e.stopPropagation()
+  }
+
+  scrollTo (index) {
+    if (!index && index !== 0) {
+      return
+    }
+
+    const children = this.listGroup.children
+    const len = children.length
+
+    if (index < 0) {
+      index = 0
+    } else if (index >= len - 1) {
+      index = len - 1
+    }
+
+    this.scroll.scrollToElement(children[index], 0)
+  }
+
+  calculateHeight () {
+    if (this.listItemHeights.length !== 0 || this.props.list.length === 0) {
+      return
+    }
+
+    let height = 0
+    const listItemHeights = [height]
+    const children = this.listGroup.children
+    
+    for (let i = 0; i < children.length - 1; i ++) {
+      height += children[i].clientHeight
+      listItemHeights.push(height)
+    }
+    this.listItemHeights = listItemHeights
+  }
+
+  componentDidMount () {
+    console.log('mount')
+    this.calculateHeight()
   }
 
   componentDidUpdate () {
     console.log('update')
-  }
-
-  shouldComponentUpdate (nextProps, nextState) {
-    console.log('current ',this.state.index)
-    console.log('next ',nextState.index)
-    if (this.state.index === nextState.index){
-      return false
-    }
-    return true
+    this.calculateHeight()
   }
 
   render () {
     const { list } = this.props
 
     return (
-      <Scroll className="list-view">
-        <ul>
+      <Scroll className="list-view" ref={scroll => this.scroll = scroll}>
+        <ul ref={listGroup => this.listGroup = listGroup}>
           {
             list.map(item => (
               <li key={item.title} className="list-group">
@@ -72,7 +112,8 @@ class ListView extends Component {
         </ul>
         <ul 
           className="nav-list"
-          onTouchStart={this.touchStartHandler}>
+          onTouchStart={this.touchStartHandler}
+          onTouchMove={this.touchMoveHandler}>
           {
             list.map((item, index) => (
               <li 
